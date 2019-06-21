a49090081d34dc1ee37b37feb785eaa58349b3cc
aleffarias <aaof@ic.ufal.br>
2019-06-21 18:45:13 -0300

    Add atributos

--- Folha-De-Pagamento/src/PerfilEmpregado.java
+++ Folha-De-Pagamento/src/PerfilEmpregado.java
@@ -1,4 +1,43 @@
 
 public class PerfilEmpregado {
-
+	protected int numeroEmpregado;
+	protected String nome;
+	protected String endereco;
+	protected int tipo;
+	protected int metodoPagamento;
+	protected boolean sindicato;
+	protected double salario;
+	
+	//======================================= Get/Set=================================================
+	
+	public int getNumeroEmpregado() {
+		return this.numeroEmpregado;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String geteEndereco() {
+		return this.endereco;
+	}
+	
+	public int getTipo() {
+		return this.tipo;
+	}
+	
+	public int getMetodoPagamento() {
+		return this.metodoPagamento;
+	}
+	
+	public boolean getSindicato() {
+		return this.sindicato;
+	}
+	
+	public double getSalario() {
+		return this.salario;
+	}
+	
+	
+	
 }
