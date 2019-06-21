7e8e796fa422fc00ac2a383d2f866b5d91fdf59d
aleffarias <aaof@ic.ufal.br>
2019-06-21 18:45:03 -0300

    Add classe Main

---
+++ Folha-De-Pagamento/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		menu();
+
+	}
+	
+	public static void menu() {
+		System.out.println("\nEscolha uma opção:");
+		System.out.println("(1) - Adicionar Empregado");
+		System.out.println("(2) - Remover Empregado");
+		System.out.println("(3) - Lançar um Cartão de Ponto");
+		System.out.println("(4) - Lançar um Resultado Venda");
+		System.out.println("(5) - Lançar uma taxa de serviço");
+		System.out.println("(6) - Alterar detalhes de um empregado");
+		System.out.println("(7) - Rodar a folha de pagamento para hoje (após rodar, avança 1 dia)");
+		System.out.println("(8) - Undo/redo");
+		System.out.println("(9) - Agenda de Pagamento");
+		System.out.println("(10) - Criação de Novas Agendas de Pagamento");
+		System.out.println("(0) - Sair");
+	}
+
+}
