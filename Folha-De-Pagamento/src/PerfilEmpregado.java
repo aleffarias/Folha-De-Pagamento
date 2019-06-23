1052f4225271ab900ed9b7799896fece64dbb9ea
aleffarias <aaof@ic.ufal.br>
2019-06-22 22:44:55 -0300

    Add gets e sets

--- Folha-De-Pagamento/src/PerfilEmpregado.java
+++ Folha-De-Pagamento/src/PerfilEmpregado.java
@@ -6,7 +6,7 @@ public class PerfilEmpregado {
 	protected int tipo;
 	protected int metodoPagamento;
 	protected boolean sindicato;
-	protected double salario;
+	protected double salario = 0;
 	
 	//======================================= Get/Set=================================================
 	
@@ -14,30 +14,61 @@ public class PerfilEmpregado {
 		return this.numeroEmpregado;
 	}
 	
+	public void setNumeroEmpregado(int numeroEmpregado) {
+		this.numeroEmpregado = numeroEmpregado;
+	}
+	
+	
 	public String getNome() {
 		return this.nome;
 	}
 	
-	public String geteEndereco() {
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public String getEndereco() {
 		return this.endereco;
 	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
 	
 	public int getTipo() {
 		return this.tipo;
 	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	
 	
 	public int getMetodoPagamento() {
 		return this.metodoPagamento;
 	}
 	
+	public void setMetodoPagamento(int metodoPagamento) {
+		this.metodoPagamento = metodoPagamento;
+	}
+	
+	
 	public boolean getSindicato() {
 		return this.sindicato;
+	}
+	
+	public void setSindicato(boolean sindicato) {
+		this.sindicato = sindicato;
 	}
 	
 	public double getSalario() {
 		return this.salario;
 	}
 	
-	
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}	
 	
 }
