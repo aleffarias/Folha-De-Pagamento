4cd47523645945dcf92ca790f885e51237d4e041
aleffarias <aaof@ic.ufal.br>
2019-06-22 22:45:17 -0300

    Add classe Funcionalidades

---
+++ Folha-De-Pagamento/src/Funcionalidades.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Funcionalidades {
+	  Scanner input = new Scanner(System.in);
+	  
+	  ArrayList<PerfilEmpregado> todosEmpregados = new ArrayList<PerfilEmpregado>();
+	  
+	  private void addEmpregado() {
+		  System.out.println("\n**  ADICIONAR EMPREGADO  **\n");
+		  
+		  PerfilEmpregado empregado = new PerfilEmpregado();
+		  
+		  
+	  }
+}
