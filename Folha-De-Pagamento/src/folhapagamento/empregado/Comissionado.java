dfc78c621859b64bff4435ac98a7ecfe0b040fd4
aleffarias <aaof@ic.ufal.br>
2019-06-24 20:30:06 -0300

    Implementando salário

--- Folha-De-Pagamento/src/folhapagamento/empregado/Comissionado.java
+++ Folha-De-Pagamento/src/folhapagamento/empregado/Comissionado.java
@@ -4,18 +4,32 @@ import java.util.Scanner;
 
 public class Comissionado extends Empregado {
 	
-	private static Scanner input;
+	Scanner input = new Scanner(System.in);
 	
+	// Construtor 
+	public Comissionado(int numeroEmpregado, String nome, String endereco, int tipo, int metodoPagamento,
+			int sindicato) {
+		super(numeroEmpregado, nome, endereco, tipo, metodoPagamento, sindicato);
+	}
 	
 	// Agenda Padrão
-	String agenda = "mensal";
-	int dia = 30;
+	private String agenda = "bi-semanal";
+	private int frequencia = 2;
 	
-	public double salario() {
+	// Salário
+	private double salario;
+
+	@Override
+	public void salario() {
+		System.out.print("\nInforme o salário:\nExemplo: 1500 - 3000.56\nR$ ");
+		double salarioTemp = input.nextDouble();
 		
-		System.out.println("Digite o salário mensal:");
-		double salario = input.nextDouble();
+		System.out.println("\nInforme o percentual de comissão:\nExemplo: 1.5 - 0.6");
+		double comissao = input.nextDouble();
 		
-		return salario;
-	}
+		
+		
+		
+	}	
+	
 }
