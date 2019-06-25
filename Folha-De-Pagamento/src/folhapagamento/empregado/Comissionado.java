513e86780a26fb6842cfdeb6a2e71b1303fb31fc
aleffarias <aaof@ic.ufal.br>
2019-06-24 23:14:06 -0300

    Métod salário funcionando

--- Folha-De-Pagamento/src/folhapagamento/empregado/Comissionado.java
+++ Folha-De-Pagamento/src/folhapagamento/empregado/Comissionado.java
@@ -12,24 +12,51 @@ public class Comissionado extends Empregado {
 		super(numeroEmpregado, nome, endereco, tipo, metodoPagamento, sindicato);
 	}
 	
+	double valorVenda;
+	
 	// Agenda Padrão
 	private String agenda = "bi-semanal";
 	private int frequencia = 2;
 	
 	// Salário
-	private double salario;
+	private double salarioComissionado;
 
 	@Override
 	public void salario() {
 		System.out.print("\nInforme o salário:\nExemplo: 1500 - 3000.56\nR$ ");
 		double salarioTemp = input.nextDouble();
 		
-		System.out.println("\nInforme o percentual de comissão:\nExemplo: 1.5 - 0.6");
+		System.out.println("\nInforme o percentual de comissão:\nExemplo: 15 para 15% 1.5 para 1.5%");
 		double comissao = input.nextDouble();
 		
+		this.salarioComissionado = salarioTemp + ((valorVenda * comissao)/100);
 		
+	}
+	
+	public void resultadoVenda() {
 		
-		
-	}	
+	}
+	
+	//======================================= Get/Set =================================================
+	
+	public String getAgenda() {
+		return agenda;
+	}
+
+	public void setAgenda(String agenda) {
+		this.agenda = agenda;
+	}
+
+	public int getFrequencia() {
+		return frequencia;
+	}
+
+	public void setFrequencia(int frequencia) {
+		this.frequencia = frequencia;
+	}
+
+	public double getSalarioComissionado() {
+		return salarioComissionado;
+	}
 	
 }
