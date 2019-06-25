83d48157d444a6a8f5ec9c364e73ae9225d0f89f
aleffarias <aaof@ic.ufal.br>
2019-06-25 00:51:00 -0300

    Reultado venda implementado

--- Folha-De-Pagamento/src/folhapagamento/empregado/Comissionado.java
+++ Folha-De-Pagamento/src/folhapagamento/empregado/Comissionado.java
@@ -12,29 +12,53 @@ public class Comissionado extends Empregado {
 		super(numeroEmpregado, nome, endereco, tipo, metodoPagamento, sindicato);
 	}
 	
-	double valorVenda;
+	// Implementar uma lista de resultados de vendas para consulta
+	
 	
 	// Agenda Padrão
 	private String agenda = "bi-semanal";
 	private int frequencia = 2;
 	
+	
 	// Salário
-	private double salarioComissionado;
-
+	private double salarioComissionadoFixo;
+	private double salarioComissionadoCom;
+	private double comissao;
+	
 	@Override
 	public void salario() {
 		System.out.print("\nInforme o salário:\nExemplo: 1500 - 3000.56\nR$ ");
 		double salarioTemp = input.nextDouble();
 		
 		System.out.println("\nInforme o percentual de comissão:\nExemplo: 15 para 15% 1.5 para 1.5%");
-		double comissao = input.nextDouble();
+		this.comissao = input.nextDouble();
 		
-		this.salarioComissionado = salarioTemp + ((valorVenda * comissao)/100);
+		this.salarioComissionadoFixo = salarioTemp;
 		
 	}
 	
 	public void resultadoVenda() {
+		System.out.println("\nDia da venda:");
+		int diaVenda = input.nextInt();
 		
+		System.out.println("\nMês da venda:");
+		int mesVenda = input.nextInt();
+		
+		System.out.println("\nAno da venda:");
+		int anoVenda = input.nextInt();
+		
+		System.out.print("\nValor da venda:\nExemplo: 1500 - 3000.56\nR$ "); 
+		double valorVenda = input.nextDouble();
+		
+		this.salarioComissionadoCom = salarioComissionadoFixo + ((valorVenda * comissao)/100);
+		
+		System.out.println("\n==================================================================================");
+		System.out.println("Nome: " + getNome() + "		Número do empregado: " + getNumeroEmpregado());
+		System.out.println("----------------------------------------------------------------------------------");
+		System.out.println("Data da venda: " + diaVenda + "/ " + mesVenda + "/" + anoVenda + "		Valor da comissão: R$ " + ((valorVenda * comissao)/100));
+		System.out.println("==================================================================================");
+		
+		System.out.println("\nResultado da venda registrado com sucesso!\n");
 	}
 	
 	//======================================= Get/Set =================================================
@@ -55,8 +79,17 @@ public class Comissionado extends Empregado {
 		this.frequencia = frequencia;
 	}
 
-	public double getSalarioComissionado() {
-		return salarioComissionado;
+	public double getSalarioComissionadoFixo() {
+		return salarioComissionadoFixo;
 	}
+
+	public double getSalarioComissionadoCom() {
+		return salarioComissionadoCom;
+	}
+
+	public double getComissao() {
+		return comissao;
+	}
+
 	
 }
