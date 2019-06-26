988f0419046220d3ddd682cffccccc3fdfdf9313
aleffarias <aaof@ic.ufal.br>
2019-06-26 01:16:10 -0300

    Add alterarDetalhe

--- Folha-De-Pagamento/src/folhapagamento/Main.java
+++ Folha-De-Pagamento/src/folhapagamento/Main.java
@@ -41,7 +41,7 @@ public class Main {
 				break;
 			
 			case 6:	
-				
+				Funcionalidades.alterarDetalhe();
 				break;
 				
 			case 0:
@@ -61,9 +61,9 @@ public class Main {
 		System.out.println("(2) - Remover Empregado");
 		System.out.println("(3) - Lançar um Cartão de Ponto");
 		System.out.println("(4) - Lançar um Resultado Venda");
-		System.out.println("(5) - Lançar uma taxa de serviço");
-		System.out.println("(6) - Alterar detalhes de um empregado");
-		System.out.println("(7) - Rodar a folha de pagamento para hoje (após rodar, avança 1 dia)");
+		System.out.println("(5) - Lançar uma Taxa de Serviço");
+		System.out.println("(6) - Alterar Detalhes de um Empregado");
+		System.out.println("(7) - Rodar a Folha de Pagamento para Hoje (após rodar, avança 1 dia)");
 		System.out.println("(8) - Undo/redo");
 		System.out.println("(9) - Agenda de Pagamento");
 		System.out.println("(10) - Criação de Novas Agendas de Pagamento");
